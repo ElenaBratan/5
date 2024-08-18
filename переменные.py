74b84fcb19381d86a0b0f5c21740a56932c0f52b
ElenaBratan <elenabratan5@gmail.com>
2024-08-18 22:02:16 +0300

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,5 @@
+kurs_name="Python"
+number_of_hours="1.5"
+DZ="12"
+time_spent="0.125"
+print("Курс:",kurs_name,"затрачено часов",number_of_hours,"всего часов:",DZ,"среднее время выполнения",time_spent,"часа.") 
