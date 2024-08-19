d08a8b00d429861b2a605ae4ef4dc86f659038a7
ElenaBratan <elenabratan5@gmail.com>
2024-08-19 20:33:36 +0300

    Update переменные.py

--- переменные.py
+++ переменные.py
@@ -1,5 +1,5 @@
 kurs_name="Python"
-number_of_hours="1.5"
-DZ="12"
-time_spent="0.125"
+number_of_hours=1.5
+DZ=12
+time_spent=Number_of_hours/DZ
 print("Курс:",kurs_name,"затрачено часов",number_of_hours,"всего часов:",DZ,"среднее время выполнения",time_spent,"часа.") 
